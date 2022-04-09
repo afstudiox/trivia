09bc9bf4d5a9a252e8748714a476426e1f3d6def
Andre Felipe <afelipes@gmail.com>
2022-04-09 17:10:39 -0300

    feat: card da pergunta concluído

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -4,7 +4,8 @@ import { connect } from 'react-redux';
 import BtnRanking from '../components/BtnRanking';
 import Header from '../components/Header';
 import { thunkGetQuestion, thunkGetToken } from '../redux/actions';
-import './Questions.css';
+// import './Questions.css';
+import '../css/questions.css';
 
 class Questions extends Component {
   constructor(props) {
@@ -55,44 +56,47 @@ class Questions extends Component {
     }
 
     return (
-      <div className="question">
+      <div className="container-page">
         <Header />
-        <div className="container-question">
-          <p data-testid="question-category">
-            { questions[0].category }
-          </p>
-          <p data-testid="question-text">
-            { questions[0].question }
-          </p>
+        <div className="container-questions-aswers row">
+          <div className="container-question col">
+            {/* troquei o elemento da categoria de p para h2 */}
+            <h2 data-testid="question-category">
+              { questions[0].category }
+            </h2>
+            <p data-testid="question-text">
+              { questions[0].question }
+            </p>
+          </div>
+          <div className="container-aswers col" data-testid="answer-options">
+            {allAnswers.map((answer, indexOf) => (
+              <button
+                type="button"
+                key={ indexOf }
+                className={ colorBorder && (
+                  questions
+                    .some((element) => element.correct_answer === answer)
+                    ? 'green' : 'red') }
+                onClick={ this.handleClick }
+                data-testid={ questions
+                  .some((element) => element
+                    .correct_answer === answer) ? 'correct-answer' : 'wrong-answer' }
+              >
+                {answer}
+              </button>
+            ))}
+            <BtnRanking />
+          </div>
+          <button
+            type="button"
+            className={ btnNext ? 'btn-next' : 'btn-next-disabled' }
+            disabled={ !btnNext }
+            onClick={ this.handleClick }
+            data-testid="btn-next"
+          >
+            Next
+          </button>
         </div>
-        <div className="container-options" data-testid="answer-options">
-          {allAnswers.map((answer, indexOf) => (
-            <button
-              type="button"
-              key={ indexOf }
-              className={ colorBorder && (
-                questions
-                  .some((element) => element.correct_answer === answer)
-                  ? 'green' : 'red') }
-              onClick={ this.handleClick }
-              data-testid={ questions
-                .some((element) => element
-                  .correct_answer === answer) ? 'correct-answer' : 'wrong-answer' }
-            >
-              {answer}
-            </button>
-          ))}
-          <BtnRanking />
-        </div>
-        <button
-          type="button"
-          className={ btnNext ? 'btn-next' : 'btn-next-disabled' }
-          disabled={ !btnNext }
-          onClick={ this.handleClick }
-          data-testid="btn-next"
-        >
-          Next
-        </button>
       </div>
     );
   }
