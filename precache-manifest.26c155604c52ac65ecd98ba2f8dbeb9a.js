a5361f1c3227a613fa729c6a01e9cdb035256d18
Andre Felipe <afelipes@gmail.com>
2022-09-25 14:56:27 -0300

    Updates

---
+++ precache-manifest.26c155604c52ac65ecd98ba2f8dbeb9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba69220adf7320908ef35477f3c4263",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "af666891d07f37b64b31",
+    "url": "/trivia/static/css/main.eaac667e.chunk.css"
+  },
+  {
+    "revision": "2e4609b00e69f80b4e29",
+    "url": "/trivia/static/js/2.24b67346.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/trivia/static/js/2.24b67346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af666891d07f37b64b31",
+    "url": "/trivia/static/js/main.b895453e.chunk.js"
+  },
+  {
+    "revision": "8cf3ddda99dc054ff6df",
+    "url": "/trivia/static/js/runtime-main.55dca74b.js"
+  }
+]);
